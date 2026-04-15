7c894be858926b198eca59892774e5392a6f6406
eyar1230-hub <eyar1230@gmail.com>
2026-04-15 16:30:27 +0300

    Create ADD_STUDENTS.sql

---
+++ ADD_STUDENTS.sql
@@ -0,0 +1,5 @@
+insert into students (id, name, age, city, grade)
+VALUES (1, 'Noa', 15, 'Tel Aviv', 92.5),
+(2, 'Liam', 16, 'Haifa', 88.0),
+(3, 'Maya', 15, 'Netanya', 95.0),
+(4, 'Omer',	17, 'Jerusalem', 81.5);
